00a2c94877e4433033e1f524bd7708a653dff25e
Uillian Gabriel Valerio Da Silva <38649488862@info.local>
2020-11-24 10:36:32 -0200

    subindo atividade

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const logar = () => {
+    alert ('${email} ${senha}');
+
+    fetch('',{
+      method : 'POST',
+      headers : {
+        'contet-type' : 'aplication/json',
+        body : {
+          email : email,
+          senha : senha
+        }
+      }
+    })
+    .then(response => response.json())
+    .then(data => alert(data));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Word Uillian!</Text>
+      
+      <Text>Email</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      //react onChange={event => setEmail(event.target.value)} 
+      onChangeText={text => onChangeText(text)} 
+      value={email }
+    />
+    <Text>Senha</Text>
+      <TextInput
+      style={{ height: 40, width : 200, borderColor: 'gray', borderWidth: 1 }}
+      //react onChange={event => setSenha(event.target.value)} 
+      onChangeText={text => onChangeText(text)}
+      secureTextEntry={true} 
+      value={senha }
+      />
+      <Button
+      onPress={logar}
+      title="login"
+      color="#841584"
+      accessibilityLabel={Login}
+      />
       <StatusBar style="auto" />
     </View>
   );
